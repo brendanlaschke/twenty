ae66f66c7fa47ca3783490073b39179a62f0f90c
brendanlaschke <brendan.laschke@googlemail.com>
2025-01-04 21:38:28 +0100

    .

--- packages/twenty-server/src/modules/messaging/message-import-manager/drivers/microsoft/services/microsoft-get-message-list.service.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/drivers/microsoft/services/microsoft-get-message-list.service.ts
@@ -7,7 +7,6 @@ import {
   PageIteratorCallback,
 } from '@microsoft/microsoft-graph-client';
 import { MailFolder } from '@microsoft/microsoft-graph-types';
-import { gmail_v1 as gmailV1 } from 'googleapis';
 
 import { MicrosoftOAuth2ClientManagerService } from 'src/modules/connected-account/oauth2-client-manager/drivers/microsoft/microsoft-oauth2-client-manager.service';
 import { ConnectedAccountWorkspaceEntity } from 'src/modules/connected-account/standard-objects/connected-account.workspace-entity';
@@ -15,18 +14,12 @@ import {
   MessageImportDriverException,
   MessageImportDriverExceptionCode,
 } from 'src/modules/messaging/message-import-manager/drivers/exceptions/message-import-driver.exception';
-import { MESSAGING_GMAIL_EXCLUDED_CATEGORIES } from 'src/modules/messaging/message-import-manager/drivers/gmail/constants/messaging-gmail-excluded-categories';
-import { MESSAGING_GMAIL_USERS_MESSAGES_LIST_MAX_RESULT } from 'src/modules/messaging/message-import-manager/drivers/gmail/constants/messaging-gmail-users-messages-list-max-result.constant';
 import { GmailClientProvider } from 'src/modules/messaging/message-import-manager/drivers/gmail/providers/gmail-client.provider';
 import { GmailGetHistoryService } from 'src/modules/messaging/message-import-manager/drivers/gmail/services/gmail-get-history.service';
-import { GmailHandleErrorService } from 'src/modules/messaging/message-import-manager/drivers/gmail/services/gmail-handle-error.service';
-import { computeGmailCategoryExcludeSearchFilter } from 'src/modules/messaging/message-import-manager/drivers/gmail/utils/compute-gmail-category-excude-search-filter.util';
-import { computeGmailCategoryLabelId } from 'src/modules/messaging/message-import-manager/drivers/gmail/utils/compute-gmail-category-label-id.util';
 import {
   GetFullMessageListResponse,
   GetPartialMessageListResponse,
 } from 'src/modules/messaging/message-import-manager/services/messaging-get-message-list.service';
-import { assertNotNull } from 'src/utils/assert';
 
 @Injectable()
 export class MicrosoftGetMessageListService {
@@ -34,7 +27,6 @@ export class MicrosoftGetMessageListService {
     private readonly microsoftOAuth2ClientManagerService: MicrosoftOAuth2ClientManagerService,
     private readonly gmailClientProvider: GmailClientProvider,
     private readonly gmailGetHistoryService: GmailGetHistoryService,
-    private readonly gmailHandleErrorService: GmailHandleErrorService,
   ) {}
 
   public async getFullMessageList(
@@ -51,7 +43,7 @@ export class MicrosoftGetMessageListService {
     const { mailFolders, syncToken: mailFolderSyncToken } =
       await this.getMailFolders(microsoftClient);
 
-    const messageIdsToFetch: { mailFolder: string; messageId: string }[] = [];
+    const messageIdsToFetch: string[] = [];
 
     for (const mailFolder of mailFolders) {
       const response: PageCollection = await microsoftClient
@@ -60,7 +52,7 @@ export class MicrosoftGetMessageListService {
         .get();
 
       const callback: PageIteratorCallback = (data: { id: string }) => {
-        messageIdsToFetch.push({ mailFolder, messageId: data.id });
+        messageIdsToFetch.push(mailFolder + '/' + data.id);
 
         return true;
       };
@@ -72,9 +64,14 @@ export class MicrosoftGetMessageListService {
       );
 
       await pageIterator.iterate();
+
+      // pageIterator.getDeltaLink();
     }
 
-    return { messageExternalIds, nextSyncCursor };
+    return {
+      messageExternalIds: messageIdsToFetch,
+      nextSyncCursor: mailFolderSyncToken || '',
+    };
   }
 
   public async getPartialMessageList(
@@ -93,15 +90,6 @@ export class MicrosoftGetMessageListService {
     const { messagesAdded, messagesDeleted } =
       await this.gmailGetHistoryService.getMessageIdsFromHistory(history);
 
-    const messageIdsToFilter = await this.getEmailIdsFromExcludedCategories(
-      gmailClient,
-      syncCursor,
-    );
-
-    const messagesAddedFiltered = messagesAdded.filter(
-      (messageId) => !messageIdsToFilter.includes(messageId),
-    );
-
     if (!nextSyncCursor) {
       throw new MessageImportDriverException(
         `No nextSyncCursor found for connected account ${connectedAccount.id}`,
@@ -110,37 +98,10 @@ export class MicrosoftGetMessageListService {
     }
 
     return {
-      messageExternalIds: messagesAddedFiltered,
+      messageExternalIds: messagesAdded,
       messageExternalIdsToDelete: messagesDeleted,
       nextSyncCursor,
     };
-  }
-
-  private async getEmailIdsFromExcludedCategories(
-    gmailClient: gmailV1.Gmail,
-    lastSyncHistoryId: string,
-  ): Promise<string[]> {
-    const emailIds: string[] = [];
-
-    for (const category of MESSAGING_GMAIL_EXCLUDED_CATEGORIES) {
-      const { history } = await this.gmailGetHistoryService.getHistory(
-        gmailClient,
-        lastSyncHistoryId,
-        ['messageAdded'],
-        computeGmailCategoryLabelId(category),
-      );
-
-      const emailIdsFromCategory = history
-        .map((history) => history.messagesAdded)
-        .flat()
-        .map((message) => message?.message?.id)
-        .filter((id) => id)
-        .filter(assertNotNull);
-
-      emailIds.push(...emailIdsFromCategory);
-    }
-
-    return emailIds;
   }
 
   private async getMailFolders(microsoftClient: Client) {
